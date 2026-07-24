b77c27427a1083ee3d48037ed29d0ce65ce3474e
HamzaAhmad416 <hamzaahmed416@gmail.com>
2026-07-24 15:44:29 +0500

    Experience added

---
+++ src/data/experience.js
@@ -0,0 +1,32 @@
+const experience = [
+  {
+    id: 1,
+    title: "Software Developer",
+    company: "Contour Software Pvt Ltd",
+    duration: "May 2023 - Present",
+    location: "Islamabad, Pakistan",
+    responsibilities: [
+      "Researched and implemented service and API-related changes.",
+      "Developed desktop and web applications.",
+      "Built features using C#, .NET, AngularJS and TypeScript.",
+      "Optimized Web APIs.",
+      "Worked with SQL Server and Entity Framework."
+    ]
+  },
+
+  {
+    id: 2,
+    title: "Junior Software Developer",
+    company: "Technevity Inc",
+    duration: "July 2020 - May 2023",
+    location: "Islamabad, Pakistan",
+    responsibilities: [
+      "Developed business logic.",
+      "Designed database structures.",
+      "Created MySQL stored procedures.",
+      "Built full-stack applications."
+    ]
+  }
+];
+
+export default experience;
